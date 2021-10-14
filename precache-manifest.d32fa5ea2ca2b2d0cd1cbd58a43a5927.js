b1abec449beb18c85bb9e25a05f3f1bcf39ce6e8
Steve Cassidy <steve.cassidy@mq.edu.au>
2021-10-14 18:13:56 +1100

    Updates

---
+++ precache-manifest.d32fa5ea2ca2b2d0cd1cbd58a43a5927.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "159179c3b2c3c37c0ccfb3adfb15783a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "37c14ae5100f5591e008",
+    "url": "./static/css/main.a8b1c732.chunk.css"
+  },
+  {
+    "revision": "8fcaa183d3f6de805fbf",
+    "url": "./static/js/2.bbe49099.chunk.js"
+  },
+  {
+    "revision": "8eed7dd1e92e997c73fd5e5077a00f1e",
+    "url": "./static/js/2.bbe49099.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37c14ae5100f5591e008",
+    "url": "./static/js/main.40da70cd.chunk.js"
+  },
+  {
+    "revision": "d88b2e29ddccb4e4fc05",
+    "url": "./static/js/runtime-main.d97a0212.js"
+  }
+]);
